cb8736db7b7a691349ab050072d77e4f0b3a5a51
argvsc47 <55107887+argvsc47@users.noreply.github.com>
2021-04-29 11:31:58 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from distutils.core import setup
 
 setup(
-  name = 'DiscoDB',         # How you named your package folder (MyLib)
-  packages = ['DiscoDB'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  name = 'DiscoDB',
+  packages = ['DiscoDB'],
+  version = '1.0',
   license='gpl-3.0',     
-  description = 'Disco is minimalist password encoded database system made in python, that emphasizes simplicity and efficiency',   # Give a short description about your library
-  author = 'argvsc47',                   # Type in your name
-  author_email = '',      # Type in your E-Mail
-  url = 'https://github.com/argvsc47/DiscoDB',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/argvsc47/DiscoDB/archive/refs/tags/v_1.0.tar.gz',    # I explain this later on
-  keywords = ['Disco', 'Database', 'light', 'secure'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  description = 'Disco is minimalist password encoded database system made in python, that emphasizes simplicity and efficiency',
+  author = 'argvsc47'
+  author_email = '',
+  url = 'https://github.com/argvsc47/DiscoDB',
+  download_url = 'https://github.com/argvsc47/DiscoDB/archive/refs/tags/v_1.0.tar.gz',
+  keywords = ['Disco', 'Database', 'light', 'secure'],
+  install_requires=[
           'cryptography',
       ],
   classifiers=[
-    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Database',
-    'License :: OSI Approved :: GNU Public License 3.0',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: GNU Public License 3.0',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
