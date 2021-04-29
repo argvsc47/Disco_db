fc36b85663005ff8ef78c71cc09fc8bd493cf683
argvsc47 <55107887+argvsc47@users.noreply.github.com>
2021-04-29 11:55:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'DiscoDB',
   packages = ['DiscoDB'],
-  version = '1.0',
+  version = '1.0.0',
   license='gpl-3.0',     
   description = 'Disco is minimalist password encoded database system made in python, that emphasizes simplicity and efficiency',
   author = 'argvsc47',
   author_email = '',
   url = 'https://github.com/argvsc47/DiscoDB',
-  download_url = 'https://github.com/argvsc47/DiscoDB/archive/refs/tags/v_1.0.tar.gz',
+  download_url = 'https://github.com/argvsc47/DiscoDB/archive/refs/tags/v_1.tar.gz',
   keywords = ['Disco', 'Database', 'light', 'secure'],
   install_requires=[
           'cryptography',
